5a6eac40adec28a02ae28e3f24dd5bfc0bac9dc0
Cameron Barnes <cameron_barnes@outlook.com>
2023-12-01 22:50:49 -0800

    Finished day 2

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -1,13 +1,43 @@
+fn parse_line(str: &str) -> (i32, i32, i32, i32) {
 
+    let mut iter = str.split(':');
+    let id = iter.next().unwrap().split(' ').last().unwrap().parse::<i32>().unwrap();
+    let games_line = iter.next().unwrap();
+
+    let mut red = 0;
+    let mut green = 0;
+    let mut blue = 0;
+
+    for blocks in games_line.split(';') {
+        for block in blocks.split(',') {
+            let mut iter = block.trim().split(' ');
+            let num = iter.next().unwrap().parse::<i32>().unwrap();
+            match iter.next().unwrap() {
+                "red" => red = i32::max(red, num),
+                "green" => green = i32::max(green, num),
+                "blue" => blue = i32::max(blue, num),
+                _ => panic!("invalid colour")
+            }
+        }
+    }
+
+    (id, red, green, blue)
+
+}
 
 fn process(input: &str) -> String {
-    todo!()
+
+    input.lines().map(|line| {
+        let (_id, r, g, b) = parse_line(line);
+        r * g * b
+    }).sum::<i32>().to_string()
+
 }
 
 fn main() {
 
     let input = include_str!("./input.txt");
-    let result = process(&input);
-    println!("Day 2 Part 1: {result}");
+    let result = process(input);
+    println!("Day 2 Part 2: {result}");
 
 }

0a517877f8fb50c132e6d89f114c2aa193a61872
Cameron Barnes <cameron_barnes@outlook.com>
2023-12-01 20:20:59 -0800

    Reorganized and prep for day 2

---
+++ day-02/src/bin/part1.rs
@@ -0,0 +1,13 @@
+
+
+fn process(input: &str) -> String {
+    todo!()
+}
+
+fn main() {
+
+    let input = include_str!("./input.txt");
+    let result = process(&input);
+    println!("Day 2 Part 1: {result}");
+
+}
